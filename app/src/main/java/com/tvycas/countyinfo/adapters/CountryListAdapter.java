2bd8b77b68107f17d019c342348a5f0656cf869f
Tomvyc <tomas.vycas111@gmail.com>
2020-08-13 14:18:54 +0300

    Add map activity

--- app/src/main/java/com/tvycas/countyinfo/adapters/CountryListAdapter.java
+++ app/src/main/java/com/tvycas/countyinfo/adapters/CountryListAdapter.java
@@ -17,17 +17,19 @@ import java.util.List;
 public class CountryListAdapter extends RecyclerView.Adapter<CountryListAdapter.CountryViewHolder> {
     private LayoutInflater inflater;
     private List<CountryBase> countriesToDisplay;
+    private OnCountryListener listener;
 
-    public CountryListAdapter(Context context, List<CountryBase> countriesToDisplay) {
+    public CountryListAdapter(Context context, List<CountryBase> countriesToDisplay, OnCountryListener listener) {
         inflater = LayoutInflater.from(context);
         this.countriesToDisplay = countriesToDisplay;
+        this.listener = listener;
     }
 
     @NonNull
     @Override
     public CountryListAdapter.CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = inflater.inflate(R.layout.country_list_item, parent, false);
-        return new CountryViewHolder(itemView);
+        return new CountryViewHolder(itemView, listener);
     }
 
     @Override
@@ -53,16 +55,29 @@ public class CountryListAdapter extends RecyclerView.Adapter<CountryListAdapter.
         notifyDataSetChanged();
     }
 
-    public class CountryViewHolder extends RecyclerView.ViewHolder {
+    public interface OnCountryListener {
+        void onCountryClick(int position);
+    }
+
+    public class CountryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private final TextView nameTextView;
         private final TextView capitalTextView;
         private final TextView popTextView;
+        private final OnCountryListener onCountryListener;
 
-        public CountryViewHolder(@NonNull View itemView) {
+        public CountryViewHolder(@NonNull View itemView, OnCountryListener onCountryListener) {
             super(itemView);
             nameTextView = itemView.findViewById(R.id.country_name);
             capitalTextView = itemView.findViewById(R.id.capital);
             popTextView = itemView.findViewById(R.id.population);
+            this.onCountryListener = onCountryListener;
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            listener.onCountryClick(getAdapterPosition());
         }
     }
 }
--- app/src/main/java/com/tvycas/countyinfo/ui/MainActivity.java
+++ app/src/main/java/com/tvycas/countyinfo/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tvycas.countyinfo.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,7 +13,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.tvycas.countyinfo.R;
 import com.tvycas.countyinfo.adapters.CountryListAdapter;
 import com.tvycas.countyinfo.model.CountryBase;
-import com.tvycas.countyinfo.model.CountryInfoWithMap;
 import com.tvycas.countyinfo.viewmodel.CountryViewModel;
 
 import java.util.List;
@@ -20,7 +20,7 @@ import java.util.List;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CountryListAdapter.OnCountryListener {
     private static final String TAG = MainActivity.class.getName();
     private CountryViewModel countryViewModel;
     private List<CountryBase> countryList;
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         countryViewModel = new ViewModelProvider(this).get(CountryViewModel.class);
-        testAdditionalCall("Portugal");
 
         setUpRecyclerView();
         observeCountryList();
@@ -51,19 +50,27 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUpRecyclerView() {
         RecyclerView recyclerView = findViewById(R.id.country_list_recyclerview);
-        countryListAdapter = new CountryListAdapter(this, countryList);
+        countryListAdapter = new CountryListAdapter(this, countryList, this);
         recyclerView.setAdapter(countryListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private void testAdditionalCall(String name) {
-        countryViewModel.getCountryInfoWithMap(name).observe(this, new Observer<CountryInfoWithMap>() {
-            @Override
-            public void onChanged(CountryInfoWithMap countryInfo) {
-                if (countryInfo != null) {
-                    Log.d(TAG, "onChanged: " + countryInfo.getBoundingBox());
-                }
-            }
-        });
+    @Override
+    public void onCountryClick(int position) {
+        Log.d(TAG, "onCountryClick: clieked " + position);
+        Intent intent = new Intent(this, CountryInfoActivity.class);
+        intent.putExtra("country_name", countryList.get(position).getName());
+        startActivity(intent);
     }
+
+//    private void testAdditionalCall(String name) {
+//        countryViewModel.getCountryInfoWithMap(name).observe(this, new Observer<CountryInfoWithMap>() {
+//            @Override
+//            public void onChanged(CountryInfoWithMap countryInfo) {
+//                if (countryInfo != null) {
+//                    Log.d(TAG, "onChanged: " + countryInfo.getBoundingBox());
+//                }
+//            }
+//        });
+//    }
 }
